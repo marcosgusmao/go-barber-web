767bce9346e4d709dbdc94cb09a0f998a9438a20
marcosgusmao <marcosgusmao@hotmail.com>
2020-06-30 11:31:06 -0300

    finalizado Front

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -93,7 +93,7 @@ const ForgotPassword: React.FC = () => {
             </Button>
           </Form>
 
-          <Link to="/signin">
+          <Link to="/">
             <FiLogIn />
             Voltar ao login
           </Link>
